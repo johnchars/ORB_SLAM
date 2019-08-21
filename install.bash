bce78167a169e1a26ee45f21013237a0779ebaae
johnchars <johnchars94@outlook.com>
2019-08-21 14:44:29 +0800

    Modified install.bash

--- install.bash
+++ install.bash
@@ -25,7 +25,8 @@ echo "source ~/slam_ws/devel/setup.bash" >> ~/.bashrc
 git clone https://github.com/johnchars/ORB_SLAM.git
 echo "ORB_SLAM has been clone"
 
-export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:${HOME}/slam_ws/src
+echo "export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:${HOME}/slam_ws/src"\
+  >> ~/.bashrc
 
 cd ~/slam_ws/src/ORB_SLAM/Thirdparty/g2o/
 mkdir build && cd build
